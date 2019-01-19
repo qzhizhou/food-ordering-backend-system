e44654b4200f4b12663959c56e81e43d2aa7ed01
zhizhouqiu <qzhizhou@bu.edu>
2019-01-19 14:18:30 -0500

    Create OrderCompleteUpdaterApplication.java

---
+++ order-complete-updater/src/main/java/demo/OrderCompleteUpdaterApplication.java
@@ -0,0 +1,15 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableCircuitBreaker
+@EnableDiscoveryClient
+public class OrderCompleteUpdaterApplication {
+    public static void main(String[] args){
+        SpringApplication.run(OrderCompleteUpdaterApplication.class,args);
+    }
+}
